124a44898fb21bb14897fcf3e070284af98ab0d9
Davis Oliveira <daviseinteligente@gmail.com>
2025-05-27 21:21:20 +0000

    generalize o problema dos hostnames
    Error: Invalid src prop (http://appcliente.com:8880/images/1d6568c4d1167a00410efd579474580c.png) on `next/image`, hostname "appcliente.com" is not configured under images in your `next.config.js`
    See more info: https://nextjs.org/docs/messages/next-image-unconfigured-host

--- src/app/app/recentes/page.tsx
+++
@@ -1,8 +0,0 @@
-
-"use client";
-
-// This page is no longer in use and can be deleted.
-// Returning null to make it an empty component.
-export default function RecentesPage() {
-  return null;
-}
